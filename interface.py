3b1051814f128e206cc69fccccea980e158581fc
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-05-16 15:30:55 +0200

    Layout change

--- interface.py
+++ interface.py
@@ -13,36 +13,28 @@ def decode_response(response: str) -> dict:
     Returns:
         dict: dictionary with response data
     """
-
     print(response)
     # Remove the surrounding single quotes from the string representation
-    return eval("{" + response + "}")
+    return eval(response)
 
 
 def write_response(response_dict: dict):
-    if (
-        response_dict.get("bar")
-        or response_dict.get("line")
-        or response_dict.get("table")
-    ):
-        if response_dict.get("bar"):
-            # Create the DataFrame from the evaluated dictionary
-            data = response_dict.get("bar")
-            df = pd.DataFrame(data)
-            df.set_index("columns", inplace=True)
-            st.bar_chart(df)
-        elif response_dict.get("line"):
-            data = response_dict.get("line")
-            df = pd.DataFrame(data)
-            df.set_index("columns", inplace=True)
-            st.line_chart(df)
-        else:
-            data = response_dict.get("table")
-            df = pd.DataFrame(data["data"], columns=data["columns"])
-            st.table(df)
-
-    else:
-        st.write(response_dict.get("answer"))
+    if "answer" in response_dict:
+        st.write(response_dict["answer"])
+    if "bar" in response_dict:
+        data = response_dict["bar"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.bar_chart(df)
+    if "line" in response_dict:
+        data = response_dict["line"]
+        df = pd.DataFrame(data)
+        df.set_index("columns", inplace=True)
+        st.line_chart(df)
+    if "table" in response_dict:
+        data = response_dict["table"]
+        df = pd.DataFrame(data["data"], columns=data["columns"])
+        st.table(df)
 
 
 st.title("👨‍💻 Chat with your CSV")
@@ -51,15 +43,10 @@ st.write("Please upload your CSV file below.")
 
 data = st.file_uploader("Upload a CSV")
 
-
-def get_query():
-    query_text = query
-    agent = create_agent(data)
-    response = query_agent(agent=agent, query=query_text)
-    decoded_response = decode_response(response)
-    write_response(decoded_response)
-
-
 query = st.text_area("Insert your query")
 
-sbt_btn = st.button("Submit Query", on_click=get_query, type="primary")
+if st.button("Submit Query", type="primary"):
+    agent = create_agent(data)
+    response = query_agent(agent=agent, query=query)
+    decoded_response = decode_response(response)
+    write_response(decoded_response)
