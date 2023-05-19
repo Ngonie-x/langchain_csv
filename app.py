a76c2d3d008cd7c5675c80ea87ddcf76c03e27de
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2023-05-19 09:34:49 +0200

    Changed prompt

--- app.py
+++ app.py
@@ -47,25 +47,29 @@ def query_agent(agent, query):
     prompt = (
         """
             For the following query, if it requires drawing a table, reply as follows:
-            "table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}
+            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
 
             If the query requires creating a bar chart, reply as follows:
-            "bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}
+            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
             
             If the query requires creating a line chart, reply as follows:
-            "line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}
+            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
             
             There can only be two types of chart, "bar" and "line".
             
             If it is just asking a question that requires neither, reply as follows:
-            "answer": "answer"
+            {"answer": "answer"}
             Example:
-            "answer": "The title with the highest rating is 'Gilead'"
+            {"answer": "The title with the highest rating is 'Gilead'"}
             
             If you do not know the answer, reply as follows:
-            "answer": "I do not know."
+            {"answer": "I do not know."}
             
             Return all output as a string.
+            
+            All strings in "columns" list and data list, should be in double quotes,
+            
+            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
             
             Lets think step by step.
             
